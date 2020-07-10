fb86c1b721a3ba4b83d8823ba881bd6e4424123b
LUCAS-SILVA <lucasmedja1@gmail.com>
2020-07-09 23:18:19 -0300

    fix

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+    <div className='py-4 text-center bg-gray-400'>
+        Lucas Silva
+    </div>
+    )
+}
+export default Footer
